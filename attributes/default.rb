05b10031bb6e86840fcc9a5d3ca4cba391609989
Nathan Cerny <ncerny@chef.io>
2015-11-12 16:43:04 -0600

    Add nfs port to firewall rules

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['nfs']['packages'] = %w( nfs-utils nfs-utils-lib )
 default['nfs']['port']['rpcbind'] = 111
+default['nfs']['port']['nfs'] = 2_049
 default['nfs']['port']['statd'] = 32_765
 default['nfs']['port']['statd_out'] = 32_766
 default['nfs']['port']['mountd'] = 32_767
